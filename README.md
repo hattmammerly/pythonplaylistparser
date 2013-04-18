40f36169a1a344f510dbc3e9a173a978985e5302
matt <sirspazzolot@gmail.com>
2013-04-18 00:25:00 -0400

    added templates folder, copied the file and made a tracks class, dun remember much else

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ still haven't learned markdown
 currently takes tracks from the itunes db and puts em in a dictionary
 takes playlists and puts em in a dictionary as well
 
-next up: play with sorting and outputting into different playlist formats
+is a dict the most time/memory-efficient way to do this?
+
+next: play with sorting and outputting into different playlist formats
 
 then maybe some ncurses action
--- parseplaylist_obj.py
+++ parseplaylist_obj.py
@@ -1,15 +1,27 @@
 from lxml import etree
+import time
+
+start = time.time();
+
+class Track:
+    __slots__ = ('name','artist','album_artist','genre','length','year','bpm','date_added','bit_rate','play_count','skip_count','purchased','location');
+    def tostring(self):
+        return "{0} - {1}".format(self.artist, self.name);
+    def __init__(self):
+        self.year = '0000';
+        self.purchased = False;
 
 def printPlaylist(key):
     print playlists[key]['name'];
     t = playlists[key]['tracks'];
     for a in t:
-        print tracks[a]['name'];
+        print tracks[a].tostring();
 
 try:
     xml = open("iTunesMusicLibrary.xml");
 #    xml = open("testing.xml");
     tree = etree.parse(xml);
+    print (time.time() - start);
 except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror);
 
@@ -25,35 +37,35 @@ playlistsxml = tree[0][-1]; #but since it's always <key/><value/> this will alwa
 tracks = {};
 for i in xrange(1,len(tracksxml), 2):
     trackno = tracksxml[i-1].text;
-    tracks[trackno] = {};
+    tracks[trackno] = Track();
     for x in xrange(0,len(tracksxml[i]), 2):
         val = tracksxml[i][x+1].text;
         if tracksxml[i][x].text == "Name":
-            tracks[trackno]['name'] = val;
+            tracks[trackno].name = val;
         elif tracksxml[i][x].text == "Artist":
-            tracks[trackno]['artist'] = val;
+            tracks[trackno].artist = val;
         elif tracksxml[i][x].text == "Album Artist":
-            tracks[trackno]['album artist'] = val;
+            tracks[trackno].album_artist = val;
         elif tracksxml[i][x].text == "Genre":
-            tracks[trackno]['genre'] = val;
+            tracks[trackno].genre = val;
         elif tracksxml[i][x].text == "Total Time":
-            tracks[trackno]['length'] = int(val)/1000; # itunes library gives time in milliseconds
+            tracks[trackno].length = int(val)/1000; # itunes library gives time in milliseconds
         elif tracksxml[i][x].text == "Year":
-            tracks[trackno]['year'] = val;
+            tracks[trackno].year = val;
         elif tracksxml[i][x].text == "BPM":
-            tracks[trackno]['bpm'] = val;
+            tracks[trackno].bpm = val;
         elif tracksxml[i][x].text == "Date Added":
-            tracks[trackno]['date added'] = val;
+            tracks[trackno].date_added = val;
         elif tracksxml[i][x].text == "Bit Rate":
-            tracks[trackno]['bit rate'] = val;
+            tracks[trackno].bit_rate = val;
         elif tracksxml[i][x].text == "Play Count":
-            tracks[trackno]['play count'] = val;
+            tracks[trackno].play_count = val;
         elif tracksxml[i][x].text == "Skip Count":
-            tracks[trackno]['skip count'] = val;
+            tracks[trackno].skip_count = val;
         elif tracksxml[i][x].text == "Purchased":
-            tracks[trackno]['purchased'] = True;
+            tracks[trackno].purchased = True;
         elif tracksxml[i][x].text == "Location":
-            tracks[trackno]['location'] = val;
+            tracks[trackno].location = val;
 
 #print len(playlistsxml);
 
