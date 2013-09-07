b338ea146522e2fb9ce09d081730be7270387d01
Matt <hattmammerly@gmail.com>
2013-09-07 18:19:14 -0400

    can convert playlists as well as the previous script; significant speed increase given small tasks

--- README.md
+++ README.md
@@ -6,8 +6,38 @@ Something to note for later: haskell's laziness may actually significantly speed
 
 So in this new version, the script uses plistlib over lxml which greatly speeds things up
 
-openLibrary(loc): feed it the location of your itunes library xml file and it will return a plistlib object
+#### openLibrary(loc):
+feed it the location of your itunes library xml file and it will return a plistlib object
 
-getTracks(library, trackIDs): given a library from above and an array of ints, returns a list of dicts of track info
+#### getTracks(library, trackIDs):
+given a library from above and an array of ints, returns a list of dicts of track info
 
-readPlaylist(library, playlistID): feeds the list of tracks from the specified playlist through getTracks()
+#### readPlaylist(library, playlistID):
+feeds the list of tracks from the specified playlist through getTracks()
+
+#### convertPlaylist(library, playlistID, form):
+converts specified playlist to the specified format from templates folder. There is an example m3u template included with the package.
+Metadata available for use in the playlist:
+- %trackName%
+- %trackArtist%
+- %trackAlbum Artist%
+- %trackLocation%
+- %trackAlbum%
+- %trackGenre%
+- %trackTotal Time%
+- %trackYear%
+- %trackBPM%
+- %trackDate Added%
+- %trackBit Rate%
+- %trackPlay Count%
+- %trackSkip Count%
+- %trackPurchased%
+
+
+I wrote this script for myself on my dual-booting system. My Windows user and Linux users share the same name and path to the music library, so I run the following command to make this playlist usable on Linux.
+
+sed -i 's/file:\/\/localhost\/C:\/Users\//\/home\//' sample.m3u && sed -i 's/%20/ /g' sample.m3u && sed -i 's/%5B/[/g' sample.m3u && sed -i 's/%5D/]/g' sample.m3u
+
+Windows users could probably get by with the following (or another utility entirely).
+
+sed -i 's/file:\/\/localhost\//' sample.m3u && sed -i 's/%20/ /g' sample.m3u && sed -i 's/%5B/[/g' sample.m3u && sed -i 's/%5D/]/g' sample.m3u
--- underthehood.py
+++ underthehood.py
@@ -22,3 +22,31 @@ def readPlaylist(library, playlistID): # playlistID must be int
     # getTracks(lib,[("{0}".format(track['Track ID'])) for track in lib['Playlists'][0]['Playlist Items']])
     return getTracks(lib,[('{0}'.format(track['Track ID'])) for track in [plist['Playlist Items'] for plist in lib['Playlists'] if plist['Playlist ID'] == playlistID][0]])
     # the comprehension does: [track number as string for every track in [the tracklist of the one playlist matching playlistID]]
+
+def convertPlaylist(library,playlistID,form):
+    try:
+        template = open('templates/{0}'.format(form))
+    except IOError as e:
+        print ('Could not open template file: templates/{0}.'.format(form))
+        return
+    try:
+        fl = open('playlists/linux/{0}.{1}'.format([plist['Name'] for plist in library['Playlists'] if plist['Playlist ID'] == playlistID][0],form),'w+') # make new playlist in linux directory. consider putting in format-specific folders too?
+        fw = open('playlists/windows/{0}.{1}'.format([plist['Name'] for plist in library['Playlists'] if plist['Playlist ID'] == playlistID][0],form),'w+') # make same playlist in windows directory
+    except IOError as e:
+        print ('Could not write playlist files')
+        return
+    tracktemplate = template.read()
+    output = tracktemplate[:tracktemplate.index('<!--TRACK-->\n')]
+    start = tracktemplate.index('<!--TRACK-->\n') + 13 # why was it +13?
+    tracktemplate = tracktemplate[start:tracktemplate.index('<!--END-->')]
+    trackinfos = ['Name','Artist','Album Artist','Location','Album','Genre','Total Time','Year','BPM','Date Added','Bit Rate','Play Count','Skip Count','Purchased']
+    for track in readPlaylist(library,playlistID):
+        entry = tracktemplate
+        for trackinfo in trackinfos:
+            try:
+                entry = entry.replace('%track{0}%'.format(trackinfo), '{0}'.format(track[trackinfo]))
+            except KeyError as e:
+                pass
+        output = output + entry
+    fl.write(output)
+    fw.write(output)
